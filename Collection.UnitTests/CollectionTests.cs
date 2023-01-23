772570e1a7368b8f711fce99d496f874789c3814
Dimo Mitev <dimosoftuni@gmail.com>
2023-01-23 21:36:51 +0200

    Add project files.

---
+++ Collection.UnitTests/CollectionTests.cs
@@ -0,0 +1,12 @@
+namespace Collection.UnitTests
+{
+    public class CollectionTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
